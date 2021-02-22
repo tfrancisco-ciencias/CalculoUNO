ae60b06eec35d5e97f461f418c282be1f8bbe244
Francisco <tfrancisco.math@gmail.com>
2021-02-22 16:57:51 -0600

    Trying dark-light mode

--- main.js
+++ main.js
@@ -6,5 +6,27 @@ $(document).ready(function() {
        $(this).next("div.resp-box").toggle();
   });
 
-
 });
+
+  function myFunction() {
+  var element = document.querySelectorAll("div.info-box");
+  document.body.classList.remove("fondo");
+  document.body.classList.add("fondo-dark");
+  var i;
+  for (i=0; i < element.length; i++ ){
+  element[i].classList.remove("info-box");
+  element[i].classList.add("info-box-dark");
+    };
+  };
+
+
+    function myFunctionL() {
+    var element = document.querySelectorAll("div.info-box-dark");
+    document.body.classList.remove("fondo-dark");
+    document.body.classList.add("fondo");
+    var i;
+    for (i=0; i < element.length; i++ ){
+    element[i].classList.remove("info-box-dark");
+    element[i].classList.add("info-box");
+      };
+    };
